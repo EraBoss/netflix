989a2fa062bb2c661dbbe914f0bff3fa13ee22de
eraboss <boss.zhagyparov@mail.ru>
2023-04-08 18:17:33 +0600

    js events

--- js/netflix1.js
+++ js/netflix1.js
@@ -3,11 +3,9 @@ const dbody = document.body;
 const spollerContainer = dbody.querySelector('.block-6__spoller-container');
 const spollerButtons = dbody.querySelectorAll('.block-6__spoller-button');
 const spollerTexts = dbody.querySelectorAll('.block-6__spoller-text');
-//language choose consts
-const langButton = dbody.querySelector('.choose-language__buttton');
-const langListItems = dbody.querySelectorAll('.choose-language__link');
-console.log(langListItems[0]);
-document.addEventListener('click', function (params) {
+//input email consts
+const inputsEmail = dbody.querySelectorAll('.block-1__input');
+spollerContainer.addEventListener('click', function (params) {
     //spoller
     if (params.target.closest('.block-6__spoller-button')) {
         if(params.target.nextElementSibling.style.maxHeight) {
@@ -16,7 +14,7 @@ document.addEventListener('click', function (params) {
                 el.style.padding = '0 24px';
                 el.previousElementSibling.classList.remove('rotate');
             })
-        } else {
+        } else { 
             spollerTexts.forEach((el) => {
                 el.style.maxHeight = null;
                 el.style.padding = '0 24px';
@@ -27,7 +25,48 @@ document.addEventListener('click', function (params) {
             params.target.classList.add('rotate');
         }
     };
+    // get started button
+    if (params.target.closest('.block-1__button-link')) {
+        if(!params.target.previousElementSibling.firstElementChild.value) {
+            params.target.previousElementSibling.firstElementChild.focus();
+        }
+    }
+});
+document.addEventListener('click', function (params) {
+    if (params.target.closest('.block-1__button-link')) {
+        if(!params.target.previousElementSibling.firstElementChild.value) {
+            params.target.previousElementSibling.firstElementChild.focus();
+        }
+    }
 })
+inputsEmail.forEach((el)=> el.addEventListener('change', function (params1) {
+    if(el.value.length <= 4) {
+        el.parentElement.lastElementChild.style.display = 'flex';
+        el.style.border = '1px solid red';
+        el.parentElement.lastElementChild.lastElementChild.innerHTML = 'Email is required!'
+    } else if(el.value.length > 4 && !el.value.includes('@') && !el.value.includes('.')) {
+        el.parentElement.lastElementChild.style.display = 'flex';
+        el.style.border = '1px solid red';
+        el.parentElement.lastElementChild.lastElementChild.innerHTML = 'Please enter valid email adress';
+    } else {
+        el.parentElement.lastElementChild.style.display = 'none';
+        el.style.border = '1px solid green';
+    }
+    el.addEventListener('input', function (params2) {
+        if(el.value.length <= 4) {
+            el.parentElement.lastElementChild.style.display = 'flex';
+            el.style.border = '1px solid red';
+            el.parentElement.lastElementChild.lastElementChild.innerHTML = 'Email is required!'
+        } else if(el.value.length > 4 && !(el.value.includes('@') && el.value.includes('.'))) {
+            el.parentElement.lastElementChild.style.display = 'flex';
+            el.style.border = '1px solid red';
+            el.parentElement.lastElementChild.lastElementChild.innerHTML = 'Please enter valid email adress';
+        } else {
+            el.parentElement.lastElementChild.style.display = 'none';
+            el.style.border = '1px solid green';
+        }
+    })
+},{once:true}))
 
 
  
