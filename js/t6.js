1d3b9c3ece213048453e5ed9571847f4f881d78c
eraboss <boss.zhagyparov@mail.ru>
2023-04-13 09:54:08 +0600

    t6

---
+++ js/t6.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView:6,
+    slidesPerGroup:6,
+    //spaceVetween: 800,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
