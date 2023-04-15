e126262328d9b4c0305c476a146df7f16079fdf8
eraboss <boss.zhagyparov@mail.ru>
2023-04-15 13:25:02 +0600

    netvideo +

--- js/netvideo.js
+++ js/netvideo.js
@@ -3,18 +3,15 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView:6,
     slidesPerGroup:6,
-    spaceBetween:10,
     // Pagination 
     pagination: {
       el: '.swiper-pagination',
     },
-  
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-  
+    }, 
   }); 
 
   const header = document.body.querySelector('.header');
@@ -22,7 +19,8 @@ const swiper = new Swiper('.swiper', {
     if(header.getBoundingClientRect().top + window.pageYOffset == 0) {
         header.style.background = 'linear-gradient(180deg,rgba(0,0,0,.7) 10%,transparent)';
     }
-  })
+  });
+
   window.addEventListener('scroll', function() {
     if(header.getBoundingClientRect().top + window.pageYOffset == 0) {
         header.style.background = 'linear-gradient(180deg,rgba(0,0,0,.7) 10%,transparent)';
