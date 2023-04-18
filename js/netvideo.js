3d03080d1bedd2077b25598fac1ae6720db737a8
eraboss <boss.zhagyparov@mail.ru>
2023-04-18 20:17:44 +0600

    adapted slider

--- js/netvideo.js
+++ js/netvideo.js
@@ -1,8 +1,8 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
-    slidesPerView:6,
-    slidesPerGroup:6,
+    slidesPerView:2,
+    slidesPerGroup:2,
     // Pagination 
     pagination: {
       el: '.swiper-pagination',
@@ -12,6 +12,24 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }, 
+    breakpoints: {
+      500: {
+        slidesPerView:3,
+        slidesPerGroup:3,
+      },
+      800: {
+        slidesPerView:4,
+        slidesPerGroup:4,
+      },
+      1100: {
+        slidesPerView:5,
+        slidesPerGroup:5,
+      },
+      1400: {
+        slidesPerView:6,
+        slidesPerGroup:6,
+      },
+    }
   }); 
 
   const header = document.body.querySelector('.header');
